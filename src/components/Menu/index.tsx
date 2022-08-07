fe675bf6a0923b25d90de3b549d68701468116b7
Luiz Otávio <luizomf@gmail.com>
2022-08-07 07:32:16 -0300

    Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './styles/global.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Menu } from './components/Menu';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
