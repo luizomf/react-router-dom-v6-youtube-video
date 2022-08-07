c96c22df69793a1583b9e85017c6ac5995858952
Luiz Otávio <luizomf@gmail.com>
2022-08-07 07:23:04 -0300

    About

---
+++ src/components/About/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Home } from './components/Home';
 import './styles/global.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
