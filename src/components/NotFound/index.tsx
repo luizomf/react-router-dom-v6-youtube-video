f5863723eed5b3533fe0f48ba5da0a2bb279a37f
Luiz Otávio <luizomf@gmail.com>
2022-08-07 07:57:15 -0300

    NotFound

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>Page not found.</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { About } from './components/About';
 import { Menu } from './components/Menu';
 import { Post } from './components/Post';
 import { Redirect } from './components/Redirect';
+import { NotFound } from './components/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
         <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
